15cb8e4d0acfb28867b3bdd985d155a185685385
Gaius Okoase Oreoluwa <gaiusonosetale.com>
2026-04-16 19:30:53 +0100

    feat: add build command to delete previous dist directory before compiling

--- dist/routes/classifyRoute.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { classifyController } from "../controllers/classifyController.js";
-import { validateQuery } from "../middleware/validation.js";
-const router = express.Router();
-router.get('/classify', validateQuery, classifyController);
-export default router;
-//# sourceMappingURL=classifyRoute.js.map
