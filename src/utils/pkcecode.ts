635c617464a54255232b02efcb8d05d9bc1ead1e
Gaius Okoase Oreoluwa <gaiusonosetale.com>
2026-04-27 19:28:37 +0100

    feat(utils):  add PKCE code_verifier and code_challenge functions

---
+++ src/utils/pkcecode.ts
@@ -0,0 +1,13 @@
+import { randomBytes, createHash }  from 'crypto';
+
+export const generateCodeVerifier = () => {
+    const buf = randomBytes(64).toString('base64url');
+    
+    return buf;
+}
+
+export const generateCodeChallenge = (code_verifier: string) => {
+    const hash = createHash('sha256').update(code_verifier).digest('base64url');
+
+    return hash;
+}
